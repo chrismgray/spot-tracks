c562ada7d94a3f31ff47f36824f55735bf375a2b
Chris Gray <chrismgray@gmail.com>
2011-11-14 13:57:12 -0700

    First commit

---
+++ src/spot_tracks/core.clj
@@ -0,0 +1,1 @@
+(ns spot-tracks.core)
---
+++ test/spot_tracks/test/core.clj
@@ -0,0 +1,6 @@
+(ns spot-tracks.test.core
+  (:use [spot-tracks.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
